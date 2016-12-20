f3f60fde7134a8b006d71b68c27e644f1d9924c3
caoyc <caoyc@getui.com>
2016-12-20 14:29:28 +0800

    first commit

---
+++ src/main/java/org/apache/hadoop/contrib/ftp/HdfsFileSystemManager.java
@@ -0,0 +1,16 @@
+package org.apache.hadoop.contrib.ftp;
+
+import org.apache.ftpserver.ftplet.FileSystemManager;
+import org.apache.ftpserver.ftplet.FileSystemView;
+import org.apache.ftpserver.ftplet.FtpException;
+import org.apache.ftpserver.ftplet.User;
+
+/**
+ * Impelented FileSystemManager to use HdfsFileSystemView
+ */
+public class HdfsFileSystemManager implements FileSystemManager {
+	public FileSystemView createFileSystemView(User user) throws FtpException {
+//		System.err.println(".................. " + user.getName());
+		return new HdfsFileSystemView(user);
+	}
+}
